b88869c3eb01ac4ff783a31e7263be31df99a36a
Tim Freese <freese.tim@gmail.com>
2020-07-06 21:06:04 -0500

    working

--- 07062020/Program.cs
+++ 07062020/Program.cs
@@ -6,7 +6,35 @@ namespace _07062020
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] testArray = new int[] { 34, -50, 42, 14, -5, 86 };
+			GetMaxSum(testArray);
+		}
+
+		public static int GetMaxSum(int[] intArray)
+		{
+			int highestSum = 0;
+			int candidate = 0;
+			
+
+			for (int i = 0; i < intArray.Length; i++)
+			{
+				for (int j = 0; j < intArray.Length; j++)
+				{
+					for (int k = i; k <= j; k++)
+					{
+						candidate += intArray[k];
+					}
+
+					if(candidate > highestSum)
+					{
+						highestSum = candidate;
+					}
+
+					candidate = 0;
+				}
+			}
+
+			return highestSum;
 		}
 	}
 }
